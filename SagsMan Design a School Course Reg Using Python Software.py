0bb5520aff8346c77637dd3cfae7fc3376a63647
Sagiru Garba <122167371+SagsMan@users.noreply.github.com>
2024-07-19 17:24:41 +0100

    Add files via upload
    This course Registration form Replica was design by SagsMan using python code

---
+++ SagsMan Design a School Course Reg Using Python Software.py
@@ -0,0 +1,77 @@
+G=input("          " "Federal University of Dutsinma,Katsina.") 
+A = " " "Bsc.Computer Science" "   " "300Level"
+Z=input("               " "Matric-No:CSA/2021/★★**★ ") 
+B=input("   " "Student Name: SagsMan") 
+print(" " "Programme: ",A)
+print("                               Private[0]Message")
+print("                                      " "Zero")
+f=[]
+f.append("Couses Outline")
+print("__________________")
+print(f)
+print("__________________")
+print("Courses in Outline 300L 2nd Semester"       "          "   " [√]" " " "[√]" " " "[√]")
+print("___________________________________________________________")
+head = []
+head.append("S/n")
+head.append("Course.code ")
+head.append("Course Title")
+head.append("#Unit")
+head.append("Category")
+print(head)
+print("___________________________________________________________")
+print("                                                           ")
+courses = []
+courses.append("♦1" "     " "GST212" "       " "Intro-to Et.Studies" "   " "2"  "      " "Core" "   ") 
+print(courses)
+courses= []
+courses.append("♦2" "     " "MTH212" "       " "Intro-to N.ANALYST" "    "  "3"  "      " "Core" "   ") 
+print(courses) 
+courses= []
+courses.append("♦3" "     " "CMP212" "       " "Computer Prog ||" "      "  "3"  "      " "Core" "   ") 
+print(courses)
+courses= []
+courses.append("♦4" "     " "CMP222" "       " "Itro to Info-Tech" "     "  "2"  "      " "Core" "   ") 
+print(courses)           
+courses= []
+courses.append("♦5" "     " "CMP232" "       " "Logic Design " "         "  "3"  "      " "Core" "   ") 
+print(courses)  
+courses= []
+courses.append("♦6" "     " "CMP242" "       " "Object Orieted Prog" "   "  "3"  "      " "Core" "   ") 
+print(courses)   
+courses= []
+courses.append("♦7" "     " "CMP252" "       " "Networks Fund&proto" "   "  "2"  "      " "Core" "   ") 
+print(courses) 
+courses= []
+courses.append("♦8" "     " "GST222" "       " "Comm_in_French" "        "  "2"  "   " "C/Optional" "") 
+print(courses)  
+courses= []
+courses.append("♦9" "     " "GST232" "       " "Comm_in_Arabic" "        "  "2"  "   " "C/Optional" "") 
+print(courses)       
+courses= []
+courses.append("♦10" "    " "CMP262" "     " "Intro to F.processing" "   "  "2"  "     " "Elective" "") 
+print(courses)      
+courses= []
+courses.append("♦11" "    " "STA212" "       " "Probability ||" "        "  "2"  "     " "Elective" "")
+print(courses)
+courses= []
+courses.append("♦12" "    " "MTH242" "       " "Linear Algebra ||" "     "  "2"  "     " "Elective" "")
+print(courses)     
+print("                    ")                   
+print("__________________________________________________________")  
+print("Required Max. credits: 24| Required Min.Credit: 18")       
+print("__________________________________________________________")   
+print("                    ")
+print("   " "______________")
+print("  " "|    Register  |")
+print("  " "|______________|")      
+print("                    ") 
+print("                             " "©2023")   
+print("                   "  " @SagsMan 🤓 👨‍💻  Code this using")
+print("                     "  "'PYTHON PROGRAMMING' SOFTWARE")
+print("                             " " 🤳🏾 08065488451") 
+print("                             " "🤳🏾 07015052112") 
+print("                                  "          "Or")
+print("                          " "SagsMan Biography @Google")  
+print("                                  "               "&")
+print("                              " "SagsMan @Youtube")
